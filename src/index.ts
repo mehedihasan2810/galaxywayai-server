54bd37bad1a5c456429a829d1fc279b8ae88859e
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-07-09 16:01:57 +0600

    test: trigger deployment

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.get("/test", async (_, res) => {
   res.status(200).send({ message: "I am aliveee...", toolsRes });
 });
 
-// start the Express server
+
 app.listen(PORT, () => {
   console.log(`Express server is running on: http://localhost:${PORT}`);
   console.log(`GraphQL server is running on: http://localhost:${PORT}/graphql`);
